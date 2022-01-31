de1850281e12789e05a0eb17837ad0e1de88bfa5
ksktada <ksk.tada@gmail.com>
2022-01-31 23:59:59 +0900

    only structure

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Profile(name: string, id: string, followers: number, followings: number) {
     return (
-        <div></div>
+        <div>
+            <div>{name}</div>
+            <div>{id}</div>
+            <div>{followers}</div>
+            <div>{followings}</div>
+        </div>
     )
 }
 
