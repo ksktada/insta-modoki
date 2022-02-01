53d5951bf28ab2c774774ba086473618c0b41b0d
ksktada <ksk.tada@gmail.com>
2022-02-01 23:59:59 +0900

    fix div to ul

--- frontend/src/components/Photo.tsx
+++ frontend/src/components/Photo.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Photo({photoData}: {photoData: string}) {
     return (
-        <div>
+        <li>
             <img src={photoData} alt=''></img>
-        </div>
+        </li>
     )
 }
 
--- frontend/src/components/Posts.tsx
+++ frontend/src/components/Posts.tsx
@@ -3,11 +3,11 @@ import Photo from './Photo';
 
 function Posts(photoList: Array<string>) {
     return (
-        <div>
+        <ul>
             {photoList.map((photoData: string, i: number) => (
                 <Photo photoData={photoData}></Photo>
             ))}
-        </div>
+        </ul>
     )
 }
 
