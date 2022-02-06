ce16ff62b8f5a40d92092d59dce9bb9699cd0919
ksktada <ksk.tada@gmail.com>
2022-02-06 23:59:59 +0900

    fix

--- frontend/src/components/Photo.tsx
+++ frontend/src/components/Photo.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function Photo({photoData}: {photoData: string}) {
+
+type Props = {
+    photoData: string
+}
+
+const Photo: React.FC<Props> = (props) => {
     return (
         <li>
             <img src='' alt={photoData}></img>
         </li>
     )
-}
+} 
 
 export default Photo;
