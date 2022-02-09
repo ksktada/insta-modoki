109dc3d05fd05c614fae7c948ca9d1fc2568e778
ksktada <ksk.tada@gmail.com>
2022-02-09 23:59:59 +0900

    add tablist

---
+++ frontend/src/components/TabList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+    selected: boolean
+}
+
+const TabList: React.FC<Props> = (props) => {
+    return (
+        <div>
+        </div>
+    )  
+}
+
+export default TabList;
